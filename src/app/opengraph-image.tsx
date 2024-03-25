23e236d937bf77c22c58a053910ca78b538249c5
Italo Pereira <italo.pereira@verndale.com>
2024-03-24 22:44:42 -0300

    testing dynamic metadata

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -22,52 +22,53 @@ export default async function Image() {
   return new ImageResponse(
     (
       // ImageResponse JSX element
-      <div 
-        className='container'
-        style={{
-          background: 'white',
-          width: '100%',
-          height: '100%',
-        }}>
+      // <div 
+      //   className='container'
+      //   style={{
+      //     background: 'white',
+      //     width: '100%',
+      //     height: '100%',
+      //   }}>
 
-        <div className='background'></div>
+      //   <div className='background'></div>
 
-        <div className='container'>
-          <div className='flex'>
-            <div className='image-box'>
-              {/* <NextImage
-                src={data.featured_img_url}
-                alt={`Foto do Presskit do ${data.title}`}
-                fill
-                style={{
-                  objectFit: 'contain',
-                  objectPosition: 'bottom center',
-                }}
-              /> */}
-              <div className='image-gradient'></div>
-            </div>
+      //   <div className='container'>
+      //     <div className='flex'>
+      //       <div className='image-box'>
+      //         {/* <NextImage
+      //           src={data.featured_img_url}
+      //           alt={`Foto do Presskit do ${data.title}`}
+      //           fill
+      //           style={{
+      //             objectFit: 'contain',
+      //             objectPosition: 'bottom center',
+      //           }}
+      //         /> */}
+      //         <div className='image-gradient'></div>
+      //       </div>
 
-            <div className='logo-textimage'>
-              <div className='logo-box'>
-                {/* <NextImage
-                  src={data.logo_url}
-                  alt={`Logo do ${data.title}`}
-                  fill
-                  style={{
-                    objectFit: 'contain',
-                    objectPosition: 'center',
-                  }}
-                /> */}
-              </div>
+      //       <div className='logo-textimage'>
+      //         <div className='logo-box'>
+      //           {/* <NextImage
+      //             src={data.logo_url}
+      //             alt={`Logo do ${data.title}`}
+      //             fill
+      //             style={{
+      //               objectFit: 'contain',
+      //               objectPosition: 'center',
+      //             }}
+      //           /> */}
+      //         </div>
 
-              <div className='text'>
-                <p>{data.featured_text}</p>
-              </div>
-            </div>
-          </div>
-        </div>
+      //         <div className='text'>
+      //           <p>{data.featured_text}</p>
+      //         </div>
+      //       </div>
+      //     </div>
+      //   </div>
         
-      </div>
+      // </div>
+      <p>{data.featured_text}</p>
     ),
     {
       ...size,
