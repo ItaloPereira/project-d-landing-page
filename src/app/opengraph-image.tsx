eeb07442a519fce10beca905e0aeddb87999b74b
Italo Pereira <italo.pereira@verndale.com>
2024-03-25 00:06:09 -0300

    testing dynamic metadata

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -1,6 +1,6 @@
-import { ImageResponse } from 'next/og'
-import NextImage from 'next/image';
- 
+import { ImageResponse } from 'next/og';
+import { headers } from 'next/headers';
+
 // Route segment config
 export const runtime = 'edge'
  
@@ -18,59 +18,35 @@ import { getPageData } from '@/data/page';
 // Image generation
 export default async function Image() {
   const data = await getPageData();
+  
+  const host = headers().get('host');
+  const protocol = headers().get('x-forwarded-proto');
  
   return new ImageResponse(
     (
-      // ImageResponse JSX element
-      // <div 
-      //   className='container'
-      //   style={{
-      //     background: 'white',
-      //     width: '100%',
-      //     height: '100%',
-      //   }}>
-
-      //   <div className='background'></div>
-
-      //   <div className='container'>
-      //     <div className='flex'>
-      //       <div className='image-box'>
-      //         {/* <NextImage
-      //           src={data.featured_img_url}
-      //           alt={`Foto do Presskit do ${data.title}`}
-      //           fill
-      //           style={{
-      //             objectFit: 'contain',
-      //             objectPosition: 'bottom center',
-      //           }}
-      //         /> */}
-      //         <div className='image-gradient'></div>
-      //       </div>
-
-      //       <div className='logo-textimage'>
-      //         <div className='logo-box'>
-      //           {/* <NextImage
-      //             src={data.logo_url}
-      //             alt={`Logo do ${data.title}`}
-      //             fill
-      //             style={{
-      //               objectFit: 'contain',
-      //               objectPosition: 'center',
-      //             }}
-      //           /> */}
-      //         </div>
-
-      //         <div className='text'>
-      //           <p>{data.featured_text}</p>
-      //         </div>
-      //       </div>
-      //     </div>
-      //   </div>
-        
-      // </div>
-      <img src={data.featured_img_url} alt={data.title} width={324} height={572} style={{
-        objectFit: 'contain'
-      }} />
+      <div
+        style={{
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundImage: `url(${protocol}://${host}${data.featured_img_url})`,
+          backgroundColor: "black",
+        }}
+      >
+        <div
+          style={{
+            marginTop: 300,
+            width: 464,
+            height: 600,
+            backgroundImage: `url(${protocol}://${host}${data.logo_url})`,
+            backgroundRepeat: "no-repeat",
+          }}
+        >
+        </div>
+      </div>
     ),
     {
       ...size,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,9 +14,16 @@ export const generateMetadata = async (): Promise<Metadata> => {
   const protocol = headers().get('x-forwarded-proto');
 
   return {
-    metadataBase: new URL(`${protocol}://${host}/`),
+    // metadataBase: new URL(`${protocol}://${host}/`),
     title: data.title,
     description: data.featured_text,
+    // openGraph: {
+    //   title: data.title,
+    //   description: data.featured_text,
+    //   type: 'website',
+    //   url: `${protocol}://${host}/`,
+    //   images: data.featured_img_url,
+    // }
   }
 }
 
